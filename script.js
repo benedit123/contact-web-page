444a460f0dcb948c1282a9c4489649aa11d9826e
benedit123 <114178640+benedit123@users.noreply.github.com>
2023-03-21 22:22:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const whats = document.getElementById("button")
+
+
+button[0].addEventListener('click', () => {
+    window.location.href = `https://whatsapp.com`;
+});
+const face = document.getElementById("button1")
+
+
+button1[1].addEventListener('click', () => {
+    window.location.href = `https://facebook.com`;
+});
+const linked = document.getElementById("button2")
+
+
+button2[0].addEventListener('click', () => {
+    window.location.href = `https://linkedin.com`;
+});
+const you = document.getElementById("button3")
+
+
+button3[1].addEventListener('click', () => {
+    window.location.href = `https://youtube.com`;
+});
+const insta = document.getElementById("button4")
+
+
+button4[0].addEventListener('click', () => {
+    window.location.href = `https://instagram.com`;
+});
